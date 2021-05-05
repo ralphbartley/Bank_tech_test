e3b9ec7844d464e7720cba6ed21ab8ce42ff1522
ralphbartley <ralphgbartley@gmail.com>
2021-05-05 16:26:55 +0100

    tests for transaction type and balance added to ledger from deposit and withdraw red

--- spec/units/bank_spec.rb
+++ spec/units/bank_spec.rb
@@ -28,6 +28,16 @@ describe Bank do
       @bank.deposit(1000)
       expect(@bank.ledger.first).to eq([1000, DateTime.now.to_date])
     end
+
+    it 'stores the type of transaction in the record' do
+      expect(@bank.ledger[1][2]).to eq("Deposit")
+    end
+
+    it 'stores the balance after the deposit' do
+      @bank.deposit(1000)
+      @bank.deposit(1000)
+      expect(@bank.ledger[1][3]).to eq(2000)
+    end
   end
 
   describe '.withdraw' do
@@ -40,7 +50,20 @@ describe Bank do
     it 'stores the date of the deposit' do
       @bank.deposit(1000)
       @bank.withdraw(500)
-      expect(@bank.ledger[1]).to eq([500, DateTime.now.to_date])
+      expect(@bank.ledger[1][0]).to eq(500)
+      expect(@bank.ledger[1][1]).to eq(DateTime.now.to_date)
+    end
+
+    it 'stores the type of transaction in the record' do
+      @bank.deposit(1000)
+      @bank.withdraw(500)
+      expect(@bank.ledger[1][2]).to eq("Withdrawal")
+    end
+
+    it 'stores the balance after the withdrawal' do
+      @bank.deposit(1000)
+      @bank.withdraw(500)
+      expect(@bank.ledger[1][3]).to eq(500)
     end
   end
 
