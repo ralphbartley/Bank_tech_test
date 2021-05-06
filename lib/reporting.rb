fd9c172a6a47b5e9874f3b3b1f0b61d06d6bf8e1
ralphbartley <ralphgbartley@gmail.com>
2021-05-06 17:45:52 +0100

    reporting format mock ledger red green

--- lib/reporting.rb
+++ lib/reporting.rb
@@ -2,7 +2,7 @@ class Reporting
 
   def self.format(ledger)
     puts 'date || credit || debit || balance'
-    ledger.each do |entry|
+    ledger.records.each do |entry|
       output = "#{entry[0]} || #{entry[1]} || #{entry[2]} || #{format('%.2f', entry[3])}"
       puts output.gsub('-', '/')
     end
--- spec/units/reporting_spec.rb
+++ spec/units/reporting_spec.rb
@@ -4,9 +4,16 @@ describe Reporting do
 
   describe '.statement' do
 
-    it 'prints the ledger contents in a date, credit, debit, balance column table' do
-
-      expect { @bank.statement }.to output(output).to_stdout
+    it 'labels the ledger contents in a date, credit, debit, balance column table' do
+      #intentional line break to make test work on puts output
+      output = 'date || credit || debit || balance
+'
+      ledger = instance_double("Ledger")
+      allow(ledger).to receive(:records)
+      allow(ledger.records).to receive(:each)
+      expect { Reporting.format(ledger) }.to output(output).to_stdout
     end
   end
 end
+
+output = 'date || credit || debit || balance'
